3796faef6fb7d2e35f159d415ece835a97c11381
Kimjenn <wogus789789@naver.com>
2021-01-24 03:40:34 +0900

    Adding Entries

---
+++ todo.js
@@ -0,0 +1,29 @@
+todoMain();
+
+function todoMain(){
+    let inputElem,
+        ulElem;
+    getElements();
+    addListeners();
+
+    function getElements(){
+        inputElem = document.getElementsByTagName("input")[0];
+        ulElem = document.getElementsByTagName("ul")[0];
+        console.log(inputElem);
+    }
+    function addListeners(){
+        inputElem.addEventListener("change", onChange, false);
+    }
+
+    function onChange(event){
+        
+        let inputValue = inputElem.value;
+
+        //ulElem.innerHTML += `<li>${inputValue}</li>`;
+        inputElem.value = "";
+        
+        let liElem = document.createElement("li");
+        liElem.innerText = inputValue;
+        ulElem.appendChild(liElem);
+    }
+}
